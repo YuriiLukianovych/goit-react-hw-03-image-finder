83a848c343c48eea014c71f7689de7758b63a610
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2023-11-17 00:22:31 +0200

    dsf

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import css from './Modal.module.scss';
 
 function Modal() {
-  return <div>Modal</div>;
+  return (
+    <div className={css.overlay}>
+      <div className={css.modal}></div>
+    </div>
+  );
 }
 
 export default Modal;
